401bbf7f24cf1426ac6a2b6490376a4d8cbdaf2e
azorlu <mandelbug@gmail.com>
2019-03-02 01:13:32 +0900

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { MenuComponent } from '../components/menu/menu.component';
+import { BoardComponent } from '../components/board/board.component';
+import { SquareComponent } from '../components/square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    BoardComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/square/square.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { faChessKing, faChessQueen, faChessRook, faChessBishop, faChessKnight, faChessPawn, faSquareFull } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.css']
+})
+export class SquareComponent implements OnInit {
+  faChessKing = faChessKing;
+  faChessQueen = faChessQueen;
+  faChessRook = faChessRook;
+  faChessBishop = faChessBishop;
+  faChessKnight = faChessKnight;
+  faChessPawn = faChessPawn;
+  faSquareFull = faSquareFull;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
